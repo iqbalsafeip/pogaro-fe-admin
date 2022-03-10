79468e20e6e34e23aefde9a01e32f530a5afdbc9
iqbal safei <isaacfrost9841@gmail.com>
2022-03-10 17:42:20 +0700

    update

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,19 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
-import { StyleSheet, View, Text, SafeAreaView, Dimensions, ScrollView, Pressable, Image } from "react-native";
-import EntypoIcon from "react-native-vector-icons/Entypo";
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, SafeAreaView, Dimensions, ScrollView, Pressable, Image, TouchableOpacity, Alert, Modal, StatusBar } from "react-native";
+
 
 import { Ionicons } from '@expo/vector-icons'
+import CardLayanan from "../../component/CardLayanan";
 
 const width = Dimensions.get('screen').width;
 const height = Dimensions.get('screen').height
 
 function Home(props) {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
       <StatusBar />
       <SafeAreaView>
+        
         <ScrollView style={{
           backgroundColor: 'white',
           width: width,
@@ -55,22 +56,50 @@ function Home(props) {
               </View>
             </View>
           </View>
+            <Modal
+              animationType="slide"
+              transparent={false}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>Hello World!</Text>
+                  <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  >
+                    <Text style={styles.textStyle}>Hide Modal</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
           <View style={[{ width: '95%', height: 140, backgroundColor: 'white', marginTop: 30, borderTopRightRadius: 20, borderBottomEndRadius: 20 }, styles.shadow]} >
             <View style={[styles.container]} >
               <View style={[styles.colCenter, { height: '100%' }]} >
                 <Text style={styles.heading} >Jam Kerja</Text>
                 <Text>10:00AM - 06:00PM</Text>
               </View>
-              <Image source={require('../../assets/img/barber.svg')} style={{width: 60, height: 60, backgroundColor: 'black'}} />
-
             </View>
           </View>
-          
+
           <View style={[styles.container, { marginTop: 20 }]} >
             <Text style={styles.heading}>
               Layanan
             </Text>
+            <View style={{ marginTop: 20, marginBottom: 20 }} >
+              <CardLayanan onBook={() => setModalVisible(true)} />
+              <CardLayanan onBook={() => setModalVisible(true)} />
+              <CardLayanan onBook={() => setModalVisible(true)} />
+            </View>
+            <TouchableOpacity style={[styles.shadow, styles.rowCenter, { width: '100%', height: 60, backgroundColor: '#F9D9A8', marginBottom: 100, borderRadius: 5 }]} >
+              <Text style={[styles.heading, { color: 'black' }]} >Review</Text>
+            </TouchableOpacity>
           </View>
+          
         </ScrollView>
       </SafeAreaView>
     </>
@@ -100,12 +129,52 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 4,
+      height: 2
     },
-    shadowOpacity: 0.30,
-    shadowRadius: 4.65,
-
-    elevation: 8,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
 });
 
